5f6d523e4745c17df4643adab85728848c978f26
Kintara10 <kintaraadipermana10@gmail.com>
2026-04-13 12:01:14 +0700

    Update Database.php

--- parkir-cafe/config/Database.php
+++ parkir-cafe/config/Database.php
@@ -7,7 +7,7 @@ class Database {
     private $host = 'localhost';
     private $dbname = 'parkir_cafe';
     private $username = 'root';
-    private $password = '';
+    private $password = 'root';
     
     private function __construct() {
         try {
